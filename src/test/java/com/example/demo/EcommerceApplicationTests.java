1f997f35af055ea3e8c4022c016f3172c85e9a9f
tapsteri <>
2020-10-06 01:44:38 +0300

    Added version info to EcommerceApplication.java

--- src/test/java/com/example/demo/EcommerceApplicationTests.java
+++ src/test/java/com/example/demo/EcommerceApplicationTests.java
@@ -5,6 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+//*******************************************************************
+//  Project: ECommerce
+//  Version: 1.0
+//  Date:    October 6, 2020
+//  Author:  Tapio K
+//
+//  Change history:
+//
+//  1.0: First version for review
+//
+//*******************************************************************
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EcommerceApplicationTests {
