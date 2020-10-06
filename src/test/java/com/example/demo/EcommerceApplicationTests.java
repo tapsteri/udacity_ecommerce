a6b213dfc3e5caaa7a55792ca2329fcfbe54af93
tapsteri <>
2020-10-06 20:30:53 +0300

    Version history updated

--- src/test/java/com/example/demo/EcommerceApplicationTests.java
+++ src/test/java/com/example/demo/EcommerceApplicationTests.java
@@ -8,13 +8,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 //*******************************************************************
 //  Project: ECommerce
-//  Version: 1.0
+//  Version: 1.1
 //  Date:    October 6, 2020
 //  Author:  Tapio K
 //
 //  Change history:
 //
 //  1.0: First version for review
+//  1.1: Screenshots jenkins_build.png added
 //
 //*******************************************************************
 
